eb034da07026d7420aca44029cee9a87ae48087c
pradip0016 <patilpradip980@gmail.com>
2020-08-09 14:39:48 +0530

    add

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -x
 #flip coin combination
-echo " flip coin  combination "
-
-check=$(( RANDOM%2 ))
-if [[ $check -eq 1 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A dict
+count=0
+heads=0
+tails=0
+for i in {1..20}
+do
+	check=$(( RANDOM%2 ))
+	if [[ $check -eq 1 ]]
+	then
+		dict[$count]="Heads"
+		heads=$(( $heads + 1 ))
+	else
+		dict[$count]="Tails"
+		tails=$(( $tails + 1 ))
+	fi
+	count=$(( $count + 1))
+done
+percentageHead=$(( $heads * 100 ))
+percentageTail=$(( $tails * 100 ))
+echo "Head Percentage: $(( percentageHead/20 ))%"
+echo "Tail Percentage: $(( percentageTail/20 ))%"
