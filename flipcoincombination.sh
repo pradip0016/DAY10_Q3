67e2442288e9efdf41ca0ff4e0a58bdd3889f1f9
pradip0016 <patilpradip980@gmail.com>
2020-08-09 15:06:12 +0530

    uc3

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,23 +1,54 @@
 #!/bin/bash -x
 #flip coin combination
+
+check=0
+HH=0
+HT=0
+TH=0
+TT=0
+counter=0
+
 declare -A dict
-count=0
-heads=0
-tails=0
+
+
 for i in {1..20}
 do
-	check=$(( RANDOM%2 ))
-	if [[ $check -eq 1 ]]
+	check1=$((RANDOM%2))
+	check2=$((RANDOM%2))
+
+	if [[ $check1 -eq 1 &&  $check2 -eq 1 ]]
 	then
-		dict[$count]="Heads"
-		heads=$(( $heads + 1 ))
+		((HH++))
+                echo "HH"
+	elif [[ $check1 -eq 0 && $check2 -eq 1 ]]
+	then
+		(( TH++ ))
+            	echo "TH" 
+
+        elif [[ $check1 -eq 1 && $check2 -eq 0 ]]
+	then
+		(( HT++ ))
+		echo "HT"
 	else
-		dict[$count]="Tails"
-		tails=$(( $tails + 1 ))
-	fi
-	count=$(( $count + 1))
+		(( TT++ ))
+		echo "TT"
+		fi
+
+			dict[HH]="HH"
+			dict[HT]="HT"
+			dict[TH]="TH"
+			dict[TT]="TT"
+
 done
-percentageHead=$(( $heads * 100 ))
-percentageTail=$(( $tails * 100 ))
-echo "Head Percentage: $(( percentageHead/20 ))%"
-echo "Tail Percentage: $(( percentageTail/20 ))%"
+
+percentageHH=$(( $HH*100 ))
+percentageHT=$(( $HT*100 ))
+percentageTH=$(( $TH*100 ))
+percentageTT=$(( $TT*100 ))
+
+echo "HH Percentage: $(( percentageHH/20 ))%"
+echo "HT Percentage: $(( percentageHT/20 ))%"
+echo "TH Percentage: $(( percentageTH/20 ))%"
+echo "TT Percentage: $(( percentageTT/20 ))%"
+
+echo "Double Combination" ${dict[@]}
